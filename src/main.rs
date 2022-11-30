9616060ae47f1c1cca64462b415c682e2c7f351d
zacgeis <zacgeis@gmail.com>
2022-11-29 18:49:06 -0600

    Add simple REPL.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use calc::eval::eval;
 use calc::parse::parse;
 
-fn main() {
-    let node = parse("1 - 2 - 3").unwrap().unwrap();
-    println!("tree: {:?}", &node);
-    println!("results: {}", eval(&node));
-    // println!("tree: {:?}, eval: {:?}", &node, eval(&node));
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let node = parse(&line).unwrap().unwrap();
+                println!("tree: {:?}", &node);
+                println!("results: {}", eval(&node));
+            },
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    Ok(())
 }
